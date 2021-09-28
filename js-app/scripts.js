b1e94bce00e8b55d82908db221e3de3bf31d85d4
Ivan <ivan.frontcoder@gmail.com>
2021-09-28 19:56:27 +0500

    .

--- js-app/scripts.js
+++ js-app/scripts.js
@@ -387,66 +387,66 @@ $(document).ready(function () {
 	// };
 	// rotate();
 
-	// // Паралакс относительно курсора мыши
-	// function parallaxMove(parallax) {
-	// 	if (parallax.length) {
-	// 		parallax.each(function () {
-	// 			var $window = $(window),
-	// 				$this = $(this),
-	// 				direction = $this.data('direction'),
-	// 				intensity = $this.data('intensity'),
-	// 				speed = $this.data('speed'),
-	// 				revers = $this.data('revers');
-	// 			if (!direction) {
-	// 				direction = 'xy';
-	// 			}
-	// 			if (!intensity) {
-	// 				intensity = 3;
-	// 			}
-	// 			if (!speed) {
-	// 				speed = 100;
-	// 			}
-	// 			if (!revers) {
-	// 				revers = false;
-	// 			}
-	// 			$this.css({ transition: (speed / 1000) + 's' });
-	// 			$window.mousemove(function (event) {
-	// 				var left = event.clientX,
-	// 					top = event.clientY,
-	// 					windowWidth = $window.width(),
-	// 					windowHeight = $window.height();
-	// 				if (revers) {
-	// 					moveX = ((left - windowWidth / 2) * intensity / 100 * -1).toFixed(),
-	// 						moveY = ((top - windowHeight / 2) * intensity / 100 * -1).toFixed();
-	// 				} else {
-	// 					moveX = ((left - windowWidth / 2) * intensity / 100).toFixed(),
-	// 						moveY = ((top - windowHeight / 2) * intensity / 100).toFixed();
-	// 				}
-	// 				inVisible($this);
-	// 				function inVisible(element) {
-	// 					var topScroll = $(document).scrollTop(),
-	// 						screenHeight = $(window).height(),
-	// 						bottomScroll = topScroll + screenHeight,
-	// 						elementHeight = element.height(),
-	// 						elementTop = element.offset().top,
-	// 						elementBottom = elementTop + elementHeight;
-	// 					if (elementTop < bottomScroll && elementBottom > topScroll) {
-	// 						if (direction == 'xy') {
-	// 							$this.css({ transform: 'translateX(' + moveX + 'px) translateY(' + moveY + 'px)' });
-	// 						}
-	// 						else if (direction == 'x') {
-	// 							$this.css({ transform: 'translateX(' + moveX + 'px)' });
-	// 						}
-	// 						else if (direction == 'y') {
-	// 							$this.css({ transform: 'translateY(' + moveY + 'px)' });
-	// 						}
-	// 					}
-	// 				};
-	// 			});
-	// 		});
-	// 	}
-	// };
-	// parallaxMove($('.js-parallaxMouse'));
+	// Паралакс относительно курсора мыши
+	function parallaxMove(parallax) {
+		if (parallax.length) {
+			parallax.each(function () {
+				var $window = $(window),
+					$this = $(this),
+					direction = $this.data('direction'),
+					intensity = $this.data('intensity'),
+					speed = $this.data('speed'),
+					revers = $this.data('revers');
+				if (!direction) {
+					direction = 'xy';
+				}
+				if (!intensity) {
+					intensity = 3;
+				}
+				if (!speed) {
+					speed = 100;
+				}
+				if (!revers) {
+					revers = false;
+				}
+				$this.css({ transition: (speed / 1000) + 's' });
+				$window.mousemove(function (event) {
+					var left = event.clientX,
+						top = event.clientY,
+						windowWidth = $window.width(),
+						windowHeight = $window.height();
+					if (revers) {
+						moveX = ((left - windowWidth / 2) * intensity / 100 * -1).toFixed(),
+							moveY = ((top - windowHeight / 2) * intensity / 100 * -1).toFixed();
+					} else {
+						moveX = ((left - windowWidth / 2) * intensity / 100).toFixed(),
+							moveY = ((top - windowHeight / 2) * intensity / 100).toFixed();
+					}
+					inVisible($this);
+					function inVisible(element) {
+						var topScroll = $(document).scrollTop(),
+							screenHeight = $(window).height(),
+							bottomScroll = topScroll + screenHeight,
+							elementHeight = element.height(),
+							elementTop = element.offset().top,
+							elementBottom = elementTop + elementHeight;
+						if (elementTop < bottomScroll && elementBottom > topScroll) {
+							if (direction == 'xy') {
+								$this.css({ transform: 'translateX(' + moveX + 'px) translateY(' + moveY + 'px)' });
+							}
+							else if (direction == 'x') {
+								$this.css({ transform: 'translateX(' + moveX + 'px)' });
+							}
+							else if (direction == 'y') {
+								$this.css({ transform: 'translateY(' + moveY + 'px)' });
+							}
+						}
+					};
+				});
+			});
+		}
+	};
+	parallaxMove($('.js-parallaxMouse'));
 
 	// // Показать еще новости
 	// function limitBlock(wrap, newsNum) {

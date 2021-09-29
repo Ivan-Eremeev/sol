0a097e9b264bb5dbdb1f67e2d86738320df6facd
Ivan <ivan.frontcoder@gmail.com>
2021-09-29 13:45:17 +0500

    .

--- js-app/scripts.js
+++ js-app/scripts.js
@@ -188,43 +188,57 @@ $(document).ready(function () {
 	// }
 	// accordion();
 
-	// // Модальное окно
-	// function modal(modal) {
-	// 	$('.modal-trigger').on('click', function() {
-	// 		var $this = $(this),
-	// 				data = $this.data('modal'),
-	// 				thisModal = $(data);
-	// 		modalShow(thisModal);
-	// 	});
-	// };
-	// // Открытие модального окна
-	// function modalShow(thisModal) {
-	// 	var html = $('html'),
-	// 			modalClose = thisModal.find($('.modal_close')),
-	// 			documentWidth = parseInt(document.documentElement.clientWidth),
-	// 			windowsWidth = parseInt(window.innerWidth),
-	// 			scrollbarWidth = windowsWidth - documentWidth;
-	// 	thisModal.show(0, function() {
-	// 		setTimeout(thisModal.addClass('open'),500);
-	// 	});
-	// 	html.addClass('lock').css('padding-right',scrollbarWidth);
-	// 	modalClose.on('click', function() {
-	// 		modalHide(thisModal);
-	// 	});
-	// 	thisModal.on('click', function(e) {
-	// 		if (thisModal.has(e.target).length === 0) {
-	// 			modalHide(thisModal);
-	// 		}
-	// 	});
-	// };
-	// // Закрытие модального окна
-	// function modalHide(thisModal) {
-	// 	var html = $('html');
-	// 	thisModal.removeClass('open');
-	// 	thisModal.hide();
-	// 	html.removeClass('lock').css('padding-right',0);
-	// };
-	// modal();
+	// Модальное окно
+	function modal(modal) {
+		$('.modal-trigger').on('click', function() {
+			var $this = $(this),
+					data = $this.data('modal'),
+					thisModal = $(data);
+			modalShow(thisModal);
+		});
+	};
+	// Открытие модального окна
+	function modalShow(thisModal) {
+		var html = $('html'),
+				modalClose = thisModal.find($('.modal_close')),
+				documentWidth = parseInt(document.documentElement.clientWidth),
+				windowsWidth = parseInt(window.innerWidth),
+				scrollbarWidth = windowsWidth - documentWidth;
+		thisModal.show(0, function() {
+			setTimeout(thisModal.addClass('open'),500);
+		});
+		html.addClass('lock').css('padding-right',scrollbarWidth);
+		modalClose.on('click', function() {
+			modalHide(thisModal);
+		});
+		thisModal.on('click', function(e) {
+			if (thisModal.has(e.target).length === 0) {
+				modalHide(thisModal);
+			}
+		});
+	};
+	// Закрытие модального окна
+	function modalHide(thisModal) {
+		var html = $('html');
+		thisModal.removeClass('open');
+		thisModal.hide();
+		html.removeClass('lock').css('padding-right',0);
+	};
+	modal();
+
+	// Вставка текста в input из выпадающего списка
+	function inputEnterText() {
+		$('.input-search').each(function () {
+			var inputBlock = $(this),
+				input = inputBlock.find('input'),
+				li = inputBlock.find('.input__list li');
+			li.on('click', function () {
+				console.log(li);
+				input.val($(this).text());
+			})
+		})
+	}
+	inputEnterText();
 
 	// // Текст печатная машинка
 	// function textPrint(block) {
